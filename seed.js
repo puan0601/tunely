4dfeb280221caf4ec27f10cc73474efbc0391b83
wilkdasilk <anwilk92@gmail.com>
2017-02-13 16:28:20 -0800

    thru sprint 3 step 2

--- seed.js
+++ seed.js
@@ -1,7 +1,28 @@
 var db = require("./models");
-var albumsList = [
+var albumsList = [];
+var sampleSongs = [];
 
-];
+sampleSongs.push({ name: 'Swamped',
+                   trackNumber: 1
+});
+sampleSongs.push({ name: "Heaven's a Lie",
+                   trackNumber: 2
+});
+sampleSongs.push({ name: 'Daylight Dancer',
+                   trackNumber: 3
+});
+sampleSongs.push({ name: 'Humane',
+                   trackNumber: 4
+});
+sampleSongs.push({ name: 'Self Deception',
+                   trackNumber: 5
+});
+sampleSongs.push({ name: 'Aeon',
+                   trackNumber: 6
+});
+sampleSongs.push({ name: 'Tight Rope',
+                   trackNumber: 7
+});
 
 albumsList.push({
   artistName: "Nine Inch Nails",
@@ -44,7 +65,9 @@ albumsList.push({
   ]
 });
 
-
+albumsList.forEach(function(album) {
+  album.songs = sampleSongs;
+});
 db.Album.remove({}, function(err, albums){
   db.Album.create(albumsList, function(err, albums){
     if(err){
